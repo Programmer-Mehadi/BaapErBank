4630e902397454548ed1439c0ab03faed1fe3c5e
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-16 13:15:42 +0600

    withdraw done

---
+++ js/withdraw.js
@@ -0,0 +1,31 @@
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    // get new Withdraw amount
+    const newWithdraw = document.getElementById('withdraw-input')
+    const newWithdrawAmount = parseFloat(newWithdraw.value)
+    // get current balance 
+    const previousBalance = document.getElementById('balance-total')
+    const previousBalanceAmount = parseFloat(previousBalance.innerText)
+
+    if (previousBalanceAmount >= newWithdrawAmount) {
+        // get previous total Withdraw
+
+        const previousWithdraw = document.getElementById('withdraw-total')
+        const previousWithdrawAmount = parseFloat(previousWithdraw.innerText)
+
+        // update Withdraw value 
+        const updateTotalWithdraw = previousWithdrawAmount + newWithdrawAmount
+        // set Withdraw
+        previousWithdraw.innerText = updateTotalWithdraw
+        // update balance 
+        previousBalance.innerText = previousBalanceAmount - newWithdrawAmount
+
+
+    }
+    else {
+        alert('Not enough balance, Please deposite...!!!')
+
+    }
+    // set withdraw input empty
+    newWithdraw.value = ''
+
+})
